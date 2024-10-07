95cc66c9a68f118965090dda79431d47cac94774
Usman Sajid <engineer.usman32@gmail.com>
2024-10-07 19:18:33 +0400

    day1 python Angela Course

---
+++ day1.py
@@ -0,0 +1,54 @@
+"""
+# video 007
+print ("Day 1 - python print function")
+print ("The function is declare like this:")
+print ("print('what to print')")
+
+# video 008
+print("Hello world!\nHello world!")
+#string concatination!
+print("Hello" + " Usman")
+print("Hello " + "Usman")
+print("Hello" +" "+ "Usman")
+
+# becareful about Identation and study about it better in python!
+print("Hello" +" "+ "Usman")
+
+video 009
+#print('e.g print("hello " + "world")')
+print("string concatination can be done '+' sign")
+"""
+#video 10 "input funtion = like scanf in c  prompt from user"
+#input("What is your name?")
+#print("Hello " + input("what is your name?"))
+
+# # video 11 exercise print length of string!
+# print(len(input("what is your name? ")))
+
+#video 12 Define variable!
+# james = input("what is your name")
+# print(james)
+
+#video 13 challenge swaping value!
+# a = input("a:")
+# b = input("b:")
+# c = 0
+# print(a)
+# print(b)
+# c = b
+# b = a
+# a = c
+# print(a)
+# print(b)
+
+#video 14 variable nameing!
+# user_name = "usmansajid786"
+# print(user_name)
+
+#video 15 Finall challenge
+print("Welcome to the band name generator")
+city_name = input("What's name of the city you grew up in?\n")
+# print(city_name)
+pet_name = input("What's your pet's name?\n")
+# print(pet_name)
+print("your brand name could be " + city_name +" " + pet_name)
