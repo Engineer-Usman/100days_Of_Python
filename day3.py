eb004c6f8ccafd3ef59b63157b05bd55b7195ec1
Usman Sajid <engineer.usman32@gmail.com>
2024-10-09 19:43:14 +0400

    3rd day -> Conditional Statement

---
+++ day3.py
@@ -0,0 +1,52 @@
+## Conditional Statements if/else if/elif/else
+##video-29
+# print("Welcome to the rollercoaster")
+# height = int(input("What is your height in cm? "))
+
+# if height > 120:
+# 	print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+##video-30
+# print("Even Odd function checking")
+# number = int(input("Enter the number: "))
+# if number % 2 == 0:
+#     print("Its Even number")
+# else:
+#     print("Its Odd number")
+
+##video-32 "Challenge"
+
+# height = float(input("Entter your height in m: "))
+# weight = float(input("Enter your weight in kg: "))
+
+# bmi = round(weight / height ** 2)
+# bmi = 18
+# if bmi > 35:
+#     print(f"Your bmi is {bmi}, and you are clinically obse")
+# elif bmi > 30:
+#     print(f"Your bmi is {bmi}, and you are Obese")
+# elif bmi > 25:
+#     print(f"Your bmi is {bmi}, and you are Overweight") 
+# elif bmi > 18.5:
+#     print(f"Your bmi is {bmi}, and you are normal weight")
+# else:
+#     print(f"Your bmi is {bmi}, and you are underweight")
+
+#video-32 "Challenge"
+
+# print("leap year challeng")
+
+# year = int(input("Enter the year: "))
+
+# if year % 4 == 0:
+# 	if year % 100 == 0:
+# 		if year % 400 == 0:
+# 			print(f"{year} it's leap year")
+# 		else:
+# 			print(f"{year} its Not a leap year")
+# 	else:
+# 		print(f"{year} its a leap year")
+# else:
+# 	print(f"{year} its Not a leap year")
