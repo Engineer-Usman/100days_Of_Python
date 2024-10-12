6960c1bd5ea3158634cd1d402799918b5c6dc437
Usman Sajid <engineer.usman32@gmail.com>
2024-10-12 15:21:12 +0400

    day4 Randomization Topic

---
+++ day4.py
@@ -0,0 +1,32 @@
+##video-40 " Randomization"
+import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# random_flaot = random.random()
+# print(random_flaot)
+
+## video - 41 "challenge flip the coin"
+# guess = random.randint(0, 1)
+# if guess == 0:
+#     print(f"Tails {guess}")
+# else:
+#     print(f"Heads {guess}")
+
+##video - 42  "List Data Structure"
+
+states_names = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+                "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+                "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
+                "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+                "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+                "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+                "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
+                "Wisconsin", "Wyoming"]
+ 
+
+print(states_names[5])
+
+states_names.append("future state")
+print(states_names)
