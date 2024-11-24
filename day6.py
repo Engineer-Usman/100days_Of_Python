fdcfefafd3f40e89e26659c5db80fd864927f968
Usman Sajid <engineer.usman32@gmail.com>
2024-11-24 17:30:47 +0400

    day 8 half done

--- day6.py
+++ day6.py
@@ -1,14 +1,1 @@
-# Maze game design
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-        
-    else:
-        turn_left()
+# Hangman game!
---
+++ day8.py
@@ -0,0 +1,62 @@
+# task one to creat greet function and type three greetings!
+# def greet():
+#     print("Good Morning")
+#     print("Good Afternoon")
+#     print("Good Night")
+
+# greet()
+
+# input in the function as paramaeter
+# def greet(name):
+#     print(f"good Morning {name}")
+#     print(f"Good Night {name}")
+    
+# greet("Usman")
+
+# Function with more parameters!
+# def greet_with(name, location):
+#     print(f"Good Morning {name} from {location}")
+#     print(f"Good Night {name} from {location}")
+
+# greet_with("ali", "UAE")
+# greet_with("Usman", "London")
+#Write your code below this line 👇
+# import math
+# def paint_calc(height, width, cover):
+#     number_of_cans = (height * width)/coverage
+#     total_cans = math.ceil(number_of_cans)
+#     print(f"total number of cans are {total_cans}")
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+###################    PRIME NUMBER CHECKING #######################
+#Write your code below this line 👇
+
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#     if is_prime:
+#         print(f"the {number} is prime number")
+#     else:
+#         print(f"the {number} is not a prime number")        
+        
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
+
+for i in range(2, 5):
+    print(i)
